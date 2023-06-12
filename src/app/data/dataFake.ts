b3241bae300ab5914a7cf7e825f3cd08acd3620b
Anderson Fernandes <anderson.fndss@gmail.com>
2023-06-12 15:15:41 -0300

    final commit

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "id": "1",
+    "title": "APPLE ANUNCIA ÓCULOS DE REALIDADE AUMENTADA",
+    "description": "Os óculos têm bateria com duração de duas horas e custarão US$ 3.499 (cerca de R$ 17,2 mil na conversão atual) nos EUA, quando chegarem ao mercado, em 2024. Não há preço divulgado para o Brasil. O preço é substancialmente maior do que os óculos de realidade virtual atualmente no mercado. Na semana passada, a Meta anunciou o seu produto, o Quest, com preço de US$ 449 R$ 2,2 mil. . A Apple usou a frase 'realidade aumentada' para descrever o novo dispositivo. A realidade aumentada, também conhecida como realidade mista, sobrepõe objetos virtuais no mundo ao nosso redor — permitindo-nos misturar realidade com o mundo virtual no nosso campo de visão. Os usuários podem acessar aplicativos, assistir a filmes e escrever documentos em um mundo virtual.",
+    "photoCover": "https://t.ctcdn.com.br/aeFrblGI46Q8J55qEoXM3VQLzOY=/1200x675/smart/filters:format(webp)/i749840.png"
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "id": "2",
+    "title": "SpaceX contrata garoto prodígio de apenas 14 anos",
+    "description": "Kairan Quazi, de 14 anos, vai ser contratado pela SpaceX a partir de julho, após se formar na faculdade de engenharia na Universidade de Santa Clara, no Estado da Califórnia, nos Estados Unidos, neste mês. As informações são do jornal Los Angeles Times. Segundo a publicação, Kairan Quazi vai trabalhar como engenheiro de software na empresa de telecomunicações do bilionário Elon Musk, responsável por construir a Starship. 'Eu acho que meus anos na faculdade foram os mais felizes da minha vida, porque eu tive muita autonomia, realmente, para compartilhar minha jornada', disse Kairan, em entrevista ao jornal. O garoto disse que vai trabalhar na Starlink, equipe de desenvolvimento de internet via satélite da SpaceX, e, com isso, vai poder 'ser parte de algo maior do que ele'.",
+    "photoCover": "https://th.bing.com/th/id/OIF.AQpD0Db3kdjBAhZFJyCmaw?pid=ImgDet&rs=1"
+  },
+  {
+    "id": "3",
+    "title": "CEO da OpenAI diz que trabalho remoto foi um erro",
+    "description": "A polêmica está no ar? Sam Altman, CEO da OpenAI, participou de uma conferência organizada pela fintech Stripe e soltou o verbo. Ele declarou textualmente ser contra o trabalho remoto e que acredita que essa tendência está com os dias contados. De acordo com o executivo, profissionais que trabalham em um escritório geram produtos para a empresa, enquanto o modelo remoto gera confusão. Altman não perdoou e meteu o dedo na ferida: “acho que definitivamente um dos piores erros da indústria de tecnologia em muito tempo foi que todo mundo pensava que poderia ser totalmente remoto para sempre“. Ele ainda disse que essa “experiência” acabou e é hora de se voltar para os escritórios.",
+    "photoCover": "https://th.bing.com/th/id/R.965aa6843bbe7194db5be0d36623543d?rik=wIa%2bBdplxxuskA&pid=ImgRaw&r=0"
+  },
+  {
+    "id": "4",
+    "title": "Produtor da série Black Mirror tentou usar ChatGPT para escrever episódio, mas ficou decepcionado com o resultado.",
+    "description": "Segundo Charlie Brooker, o sistema apenas criou um roteiro misturando informações das sinopses da série, sem adicionar “nenhum pensamento original”. A ferramenta, no entanto, o ajudou a perceber como poderia subverter as expectativas do que é esperado de um episódio da série. As informações são do site TechCrunch.",
+    "photoCover": "https://th.bing.com/th/id/R.0ad60c0d2782f223001852af61042c2d?rik=myQqzDN%2bZOZtfA&riu=http%3a%2f%2fksassets.timeincuk.net%2fwp%2fuploads%2fsites%2f55%2f2017%2f11%2f2017_CharlieBrooker_Rex_291117.jpg&ehk=c5Ebm0eKj9hbtY4Nj5qOIqBqJodfmZ44Oykg7xkOtBE%3d&risl=&pid=ImgRaw&r=0"
   }
 ]
